a59d10a20a4d2c6796b264e1e10ab90b176b66bb
OllegK <oleg.koleskin@gmail.com>
2019-09-19 07:16:32 +0300

    typo fixed

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm ./output/*.png
 
-docker build -t olegk/bot_poc_wind .
+# docker build -t olegk/bot_poc_wind .
 
-docker run -it -e ID='bot1' -v ~/bot_poc/output:/usr/src/app/output bot_poc_wind 
+docker run -it -e ID='bot1' -v ~/bot_poc/output:/usr/src/app/output olegk/bot_poc_wind 
 # --network "container:047cb1f23aad" 
